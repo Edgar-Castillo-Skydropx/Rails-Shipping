52ec91e8e8b903ce28b3505ab66cafa1774581f0
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2024-12-27 14:25:11 -0600

    feat: some features were implemented and fixes were added on all the modules [v1]

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="notification"
 export default class extends Controller {
-  static targets = ["listdown", "dialog", "dialogContent"];
+  static targets = ["listdown", "dialog", "dialogContent", "dialogBox"];
 
   toggle(e) {
     e.preventDefault();
@@ -24,15 +24,29 @@ export default class extends Controller {
     // Actualizar el contenido del diálogo
     this.dialogContentTarget.innerHTML = `
       <p><strong>Título:</strong> ${notificationData.title}</p>
-      <p><strong>Mensaje:</strong> ${notificationData.content}</p>
+      <p><strong>Mensaje:</strong> ${notificationData.body}</p>
       <p><strong>Fecha:</strong> ${notificationData.created_at}</p>
     `;
 
-    // Mostrar el diálogo
-    this.dialogTarget.classList.remove("hidden");
+    // Mostrar el diálogo con transiciones
+    this.dialogTarget.classList.remove("hidden", "opacity-0");
+    this.dialogTarget.classList.add("opacity-100");
+
+    this.dialogBoxTarget.classList.remove("scale-95");
+    this.dialogBoxTarget.classList.add("scale-100");
   }
 
   closeDialog() {
-    this.dialogTarget.classList.add("hidden");
+    // Aplicar transiciones de salida
+    this.dialogTarget.classList.add("opacity-0");
+    this.dialogTarget.classList.remove("opacity-100");
+
+    this.dialogBoxTarget.classList.add("scale-95");
+    this.dialogBoxTarget.classList.remove("scale-100");
+
+    // Esconder el diálogo después de la transición
+    setTimeout(() => {
+      this.dialogTarget.classList.add("hidden");
+    }, 300); // Debe coincidir con `duration-300`
   }
 }

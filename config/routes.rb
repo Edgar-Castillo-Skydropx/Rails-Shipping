6edfac053fbeebef79bdcbbefe248fa4d3d7d5bb
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2024-12-28 12:18:45 -0600

    feat: some features were implemented and fixes were added on all the modules [v1]

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   get "home/index"
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   scope :admin do
     authenticated do
       resources :notifications, except: [ :public_notifications, :show ]
+      resources :users
     end
   end
   authenticated do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_23_195958) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_28_180618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_23_195958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.boolean "isAdmin", default: true
+    t.boolean "isAdmin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true

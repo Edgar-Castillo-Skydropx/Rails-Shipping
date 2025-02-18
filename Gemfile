78442d9d834aea385e3c68fba5c549d9b53cb498
Edgar Castillo Vega <edgar.castillo@skydropx.com>
2025-02-18 11:25:32 -0600

    feat: some features were implemented and fixes were added on all the modules [v1]

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem "devise", "~> 4.9"
 
 gem "tailwind_devise", "~> 0.0.3"
 
+gem 'pry', '~> 0.14.2'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ], require: "debug/prelude"
--- app/controllers/quoter_controller.rb
+++ app/controllers/quoter_controller.rb
@@ -1,18 +1,16 @@
 class QuoterController < ApplicationController
-  before_action :set_tab_uuid, only: :home
-
   def home
   end
 
   def quote
-    tab_uuid = params[:tab_uuid]
+    @tab_uuid = params[:tab_uuid]
 
-    return head :bad_request if tab_uuid&.blank?
+    return head :bad_request if !@tab_uuid
 
     @quotation = generate_quotation(current_user)
 
     BroadcastService.broadcast_to(
-      :broadcast_prepend_to, "carrier_quotation_#{dom_id(current_user)}",
+      :broadcast_prepend_to, "carrier_quotation_#{@tab_uuid}",
       target: "carrier_quotation", partial: "quoter/quoter",
       locals: { quotation: @quotation }
     )
@@ -21,9 +19,6 @@ class QuoterController < ApplicationController
   end
 
   private
-  def set_tab_uuid
-    gon.tab_uuid = 55
-  end
 
   def generate_quotation(user)
     { quote: "12345", user: user }
